71595d6f0b6b0a60e26b458662ab19ad9a108da8
Chris Rutkowski <chris@rkw.ski>
2025-09-24 18:46:17 +0400

    no message

---
+++ example/test/e2e_ecom_test.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:example/features/products/data/product_dto.dart';
+import 'package:example/features/products/data/rating_dto.dart';
+import 'package:example/features/products/presentation/products_keys.dart';
+import 'package:example/features/welcome/presentation/welcome_keys.dart';
+import 'package:taptest/taptest.dart';
+
+import '_utils/default_tap_tester_config.dart';
+
+// const standardTapTesterConfig =
+
+void main() {
+  final config = defaultTapTesterConfig.copyWith(
+    httpRequestHandlers: [
+      // const ImageHandler(),
+      const ProductsHandler(
+        products: [
+          ProductDto(
+            id: 1,
+            title: 'Test Product 1',
+            price: 29.99,
+            description: 'Description for Test Product 1',
+            category: 'electronics',
+            image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+            rating: RatingDto(rate: 4.5, count: 120),
+          ),
+        ],
+      ),
+    ],
+    suite: 'e2e_ecom',
+  );
+
+  tapTest('flow', config, (tester) async {
+    await tester.info('On Welcome screen');
+    await tester.exists(WelcomeKeys.screen);
+    // await tester.snapshot('welcome_screen');
+    await tester.tap(WelcomeKeys.productsButton, sync: SyncType.settled);
+
+    await tester.info('On Products screen');
+    await tester.exists(ProductScreenKeys.screen);
+    // await tester.snapshot('http_screen');
+
+    // await tester.absent(LongKeys.cell(72));
+    // await tester.scrollTo([LongKeys.cell(72), LongKeys.cellTitle], scrollable: LongKeys.list);
+    // await tester.snapshot('long_screen_72');
+    await tester.snapshot('http_screen');
+
+    // await tester.pop();
+
+    // await tester.snapshot('long_screen_popped');
+  });
+}
+
+final class ProductsHandler implements MockHttpRequestHandler {
+  final List<ProductDto> products;
+
+  const ProductsHandler({
+    required this.products,
+  });
+
+  @override
+  bool canHandle(Uri uri, HttpMethod method, String path) {
+    return method == HttpMethod.get && uri.path == '/products';
+  }
+
+  @override
+  MockHttpResponse? handle(Uri uri, HttpHeaders headers, String? body) {
+    return MockHttpResponse(
+      headers: {
+        // not truly needed
+        HttpHeaders.contentTypeHeader: 'application/json',
+      },
+      body: jsonEncode(products),
+    );
+  }
+}
---
+++ lib/src/stubs/qr_network_image_stub.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:qr/qr.dart';
+
+Widget defaultWidgetQRStubBuilder(String src, double? width, double? height, BoxFit? fit) {
+  return WidgetQRStub(
+    src: src,
+    width: width,
+    height: height,
+    fit: fit,
+  );
+}
+
+final class WidgetQRStub extends StatelessWidget {
+  static const _defaultSize = 32.0;
+
+  final String src;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
+
+  const WidgetQRStub({
+    super.key,
+    required this.src,
+    this.width,
+    this.height,
+    this.fit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final qrCode = QrCode.fromData(
+      // data: src,
+      data: Object.hash(src, width, height, fit).toString(),
+      errorCorrectLevel: QrErrorCorrectLevel.L,
+    );
+
+    return Container(
+      color: Colors.white,
+      width: width ?? _defaultSize,
+      height: height ?? _defaultSize,
+      child: Center(
+        child: AspectRatio(
+          aspectRatio: 1.0,
+          child: CustomPaint(
+            painter: _Painter(QrImage(qrCode)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final class _Painter extends CustomPainter {
+  final QrImage qrImage;
+
+  const _Painter(this.qrImage);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.fill;
+
+    final moduleCount = qrImage.moduleCount;
+    final moduleSize = size.width / (moduleCount + 2);
+
+    for (int x = 0; x < moduleCount; x++) {
+      for (int y = 0; y < moduleCount; y++) {
+        if (!qrImage.isDark(y, x)) continue;
+
+        final rect = Rect.fromLTWH(
+          (x + 1) * moduleSize,
+          (y + 1) * moduleSize,
+          moduleSize,
+          moduleSize,
+        );
+        canvas.drawRect(rect, paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate is! _Painter || oldDelegate.qrImage != qrImage;
+  }
+}
