137b95d19f20f86a7e59df8c9f27d89a4b0c4458
Chris Rutkowski <chris@rkw.ski>
2025-09-26 12:56:45 +0400

    no message

---
+++ example/test/folder/disappearing_test.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:example/features/disappearing_widget_screen.dart/disappearing_keys.dart';
+import 'package:example/features/welcome/presentation/welcome_keys.dart';
+import 'package:taptest/taptest.dart';
+import 'package:test/test.dart';
+import '../_utils/default_tap_tester_config.dart';
+
+void main() {
+  tapTest('Disappearing Widget will disappear', defaultTapTesterConfig, (tester) async {
+    await tester.info('On Welcome screen');
+    await tester.exists(WelcomeKeys.screen);
+    await tester.tap(WelcomeKeys.disappearingButton, sync: SyncType.settled);
+
+    await tester.info('On Disappearing Widget screen');
+    await tester.exists(DisappearingKeys.screen);
+    await tester.exists(DisappearingKeys.disappearingWidget);
+    await tester.snapshot('before');
+    await tester.wait(Duration(seconds: 2));
+    await tester.absent(DisappearingKeys.disappearingWidget);
+    await tester.snapshot('after 2 seconds');
+  });
+}
